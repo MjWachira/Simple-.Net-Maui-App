f2e2778b59d14976636fa4357ee1394198c6c492
mjwachira <mjwachira1@gmail.com>
2024-06-03 18:28:38 +0300

    Nav Menu Adjusted.

--- MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4f0434235bad1fda70efb847a98dc95bd80008ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
